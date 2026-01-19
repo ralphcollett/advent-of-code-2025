0373cba7bc0f8c37b5269d12f7644c4451a80fd3
ralph.collett <ralph.collett@gmail.com>
2026-01-19 14:53:45 +0000

    Move ID valid checker to production code.

--- src/test/kotlin/day2/InvalidIdDetectorTest.kt
+++ src/test/kotlin/day2/InvalidIdDetectorTest.kt
@@ -28,16 +28,4 @@ class InvalidIdDetectorTest {
         assertFalse(idInvalid(12))
     }
 
-    private fun idInvalid(productID: Int): Boolean {
-        val digits = generateSequence(productID / 10 to productID % 10)
-            { it.first / 10 to it.first % 10 }
-            .takeWhile { it.first > 0 || it.second > 0 }
-            .map { it.second }
-            .toList()
-            .reversed()
-
-        val firstHalf = digits.subList(0, digits.size / 2)
-        val secondHalf = digits.subList(digits.size / 2, digits.size)
-        return firstHalf == secondHalf
-    }
 }
