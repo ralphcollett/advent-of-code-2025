025cb23eb7af97fe7dcc0cd203931fd8426934b6
ralph.collett <ralph.collett@gmail.com>
2026-01-19 15:16:36 +0000

    Parse multiple ranges.

--- src/test/kotlin/day2/InvalidIdRangeTextParserTest.kt
+++ src/test/kotlin/day2/InvalidIdRangeTextParserTest.kt
@@ -10,8 +10,36 @@ class InvalidIdRangeTextParserTest {
         assertEquals(listOf(IdRange(11, 22)), parse("11-22"))
     }
 
+    @Test
+    fun `Parses multiple ranges`() {
+        val ranges = parse(
+            """
+            11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+            1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+            824824821-824824827,2121212118-2121212124
+        """.trimIndent()
+        )
+
+        assertEquals(listOf(
+            IdRange(11, 22),
+            IdRange(95, 115),
+            IdRange(998, 1012),
+            IdRange(1188511880, 1188511890),
+            IdRange(222220, 222224),
+            IdRange(1698522, 1698528),
+            IdRange(446443, 446449),
+            IdRange(38593856, 38593862),
+            IdRange(565653, 565659),
+            IdRange(824824821, 824824827),
+            IdRange(2121212118, 2121212124),
+        ), ranges)
+    }
+
     private fun parse(inputString: String): List<IdRange> {
-        val rangeIds = inputString.split("-")
-        return listOf(IdRange(rangeIds[0].toInt(), rangeIds[1].toInt()))
+        val rangesInputs = inputString.split(',')
+        return rangesInputs.map { rangeInput ->
+            val rangeIds = rangeInput.split("-").map { it.trim() }
+            IdRange(rangeIds[0].toInt(), rangeIds[1].toInt())
+        }
     }
 }
