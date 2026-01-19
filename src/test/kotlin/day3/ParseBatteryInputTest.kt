2d377c49a75d10ff217de715543dfa4d48bed91b
ralph.collett <ralph.collett@gmail.com>
2026-01-19 16:00:42 +0000

    Parse multiple banks.

--- src/test/kotlin/day3/ParseBatteryInputTest.kt
+++ src/test/kotlin/day3/ParseBatteryInputTest.kt
@@ -11,15 +11,24 @@ class ParseBatteryInputTest {
     fun `Parses the puzzle input`() {
         assertEquals(
             listOf(
-                Bank(listOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1))
+                Bank(listOf(9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1)),
+                Bank(listOf(8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9)),
+                Bank(listOf(2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8)),
+                Bank(listOf(8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1)),
             ),
-            parse("987654321111111")
+            parse("""
+                987654321111111
+                811111111111119
+                234234234234278
+                818181911112111
+            """.trimIndent())
         )
     }
 
     private fun parse(puzzleInput: String): List<Bank> {
-        return listOf(
-            Bank(puzzleInput.map { it.digitToInt() })
-        )
+        return puzzleInput.split("\n").map {
+            bankInput ->
+                Bank(bankInput.map { it.digitToInt() })
+        }
     }
 }
