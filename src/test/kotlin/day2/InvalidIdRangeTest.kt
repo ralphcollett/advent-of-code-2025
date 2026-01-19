30afbb1c10a77cdc3be0479d8822eba78e0d39c1
ralph.collett <ralph.collett@gmail.com>
2026-01-19 15:04:44 +0000

    Test more invalid ID ranges.

--- src/test/kotlin/day2/InvalidIdRangeTest.kt
+++ src/test/kotlin/day2/InvalidIdRangeTest.kt
@@ -8,6 +8,8 @@ class InvalidIdRangeTest {
     @Test
     fun `Finds invalid IDs in a range`() {
         assertEquals(listOf(11, 22), findInvalidIds(IdRange(11, 22)))
+        assertEquals(listOf(1188511885), findInvalidIds(IdRange(1188511880, 1188511890)))
+        assertEquals(emptyList(), findInvalidIds(IdRange(1698522, 1698528)))
     }
 }
 
