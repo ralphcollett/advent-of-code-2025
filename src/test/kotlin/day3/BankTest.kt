bdfa4da7a5a08ddeee795e1a85cb5567969fa57f
ralph.collett <ralph.collett@gmail.com>
2026-01-19 16:20:09 +0000

    Test for bank with less than 2 batteries.

--- src/test/kotlin/day3/BankTest.kt
+++ src/test/kotlin/day3/BankTest.kt
@@ -16,8 +16,15 @@ class BankTest {
         )
     }
 
+    @Test
+    fun `No joltage if less than 2 batteries`() {
+        assertEquals(0, joltage(Bank(emptyList())))
+        assertEquals(0, joltage(Bank(listOf(2))))
+    }
+
     private fun joltage(bank: Bank): Int {
         val batteries = bank.batteries
+        if (batteries.size < 2) return 0
         val firstDigit = batteries.subList(0, batteries.size - 1).max()
         val secondDigit = batteries.subList(batteries.indexOf(firstDigit) + 1, batteries.size).max()
         return firstDigit * 10 + secondDigit
