2fbe09aa8415916340ae9c59d5dabdcd904b3020
ralph.collett <ralph.collett@gmail.com>
2026-01-19 08:23:32 +0000

    Move acceptnace test to different file.

---
+++ src/test/kotlin/day1/PasswordAcceptanceTest.kt
@@ -0,0 +1,43 @@
+package day1
+
+import day1.Direction.LEFT
+import day1.Direction.RIGHT
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Disabled
+import kotlin.test.Test
+
+class PasswordAcceptanceTest {
+
+    /*
+     * L68
+     * L30
+     * R48
+     * L5
+     * R60
+     * L55
+     * L1
+     * L99
+     * R14
+     * L82
+     * Password = 3
+     */
+    @Disabled
+    @Test
+    fun `Get's the password`() {
+        assertEquals(
+            3, password(
+                Rotation(LEFT, 68),
+                Rotation(LEFT, 30),
+                Rotation(RIGHT, 48),
+                Rotation(LEFT, 5),
+                Rotation(RIGHT, 60),
+                Rotation(LEFT, 55),
+                Rotation(LEFT, 1),
+                Rotation(LEFT, 99),
+                Rotation(RIGHT, 14),
+                Rotation(LEFT, 82),
+            )
+        )
+    }
+
+}
