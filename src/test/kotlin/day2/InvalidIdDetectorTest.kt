e35df93d161d2059d36b4914958256b7cdd722cf
ralph.collett <ralph.collett@gmail.com>
2026-01-19 14:52:11 +0000

    Test if valid for multiple digits.

--- src/test/kotlin/day2/InvalidIdDetectorTest.kt
+++ src/test/kotlin/day2/InvalidIdDetectorTest.kt
@@ -7,8 +7,13 @@ import kotlin.test.assertFalse
 class InvalidIdDetectorTest {
 
     @Test
-    fun `Knows if ID invalid`() {
+    fun `Knows if ID invalid 2 digit number`() {
         assertTrue(idInvalid(11))
+    }
+
+    @Test
+    fun `Knows if ID invalid 4 digit number`() {
+        assertTrue(idInvalid(1010))
     }
 
     @Test
@@ -17,6 +22,15 @@ class InvalidIdDetectorTest {
     }
 
     private fun idInvalid(productID: Int): Boolean {
-        return productID % 10 == productID / 10
+        val digits = generateSequence(productID / 10 to productID % 10)
+            { it.first / 10 to it.first % 10 }
+            .takeWhile { it.first > 0 || it.second > 0 }
+            .map { it.second }
+            .toList()
+            .reversed()
+
+        val firstHalf = digits.subList(0, digits.size / 2)
+        val secondHalf = digits.subList(digits.size / 2, digits.size)
+        return firstHalf == secondHalf
     }
 }
