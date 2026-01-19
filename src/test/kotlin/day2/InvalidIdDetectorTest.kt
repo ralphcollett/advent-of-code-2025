2914ed99ac7f06d539c7020bf447e3965cb60b43
ralph.collett <ralph.collett@gmail.com>
2026-01-19 14:53:15 +0000

    More test cases.

--- src/test/kotlin/day2/InvalidIdDetectorTest.kt
+++ src/test/kotlin/day2/InvalidIdDetectorTest.kt
@@ -9,11 +9,18 @@ class InvalidIdDetectorTest {
     @Test
     fun `Knows if ID invalid 2 digit number`() {
         assertTrue(idInvalid(11))
+        assertTrue(idInvalid(99))
     }
 
     @Test
     fun `Knows if ID invalid 4 digit number`() {
         assertTrue(idInvalid(1010))
+    }
+
+    @Test
+    fun `Knows if ID invalid for many digits`() {
+        assertTrue(idInvalid(1188511885))
+        assertTrue(idInvalid(222222))
     }
 
     @Test
