f0de35c4e767ea9d972228f4515fc37fa970c753
ralph.collett <ralph.collett@gmail.com>
2026-01-22 09:46:34 +0000

    Test parsing maths.

--- src/test/kotlin/day6/CephalodMathsInputParserTest.kt
+++ src/test/kotlin/day6/CephalodMathsInputParserTest.kt
@@ -1,11 +1,12 @@
 package day6
 
-import day6.CephalodMathsOperator.MULTIPLIED
+import day6.CephalodMathsOperator.ADDITION
+import day6.CephalodMathsOperator.MULTIPLICATION
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 enum class CephalodMathsOperator {
-    PLUS, MULTIPLIED
+    ADDITION, MULTIPLICATION
 }
 
 data class CephalodMathsProblem(val numbers: List<Int>, val operator: CephalodMathsOperator)
@@ -17,33 +18,38 @@ class CephalodMathsInputParserTest {
     @Test
     fun `Parses maths input`() {
         val puzzleInput = """
-            123
-             45
-              6
-            *  
+            123 328  51 64 
+             45 64  387 23 
+              6 98  215 314
+            *   +   *   +   
         """.trimIndent()
 
         assertEquals(
             CephalodMaths(
                 listOf(
-                    CephalodMathsProblem(
-                        listOf(123, 45, 6),
-                        MULTIPLIED
-                    )
+                    CephalodMathsProblem(listOf(123, 45, 6), MULTIPLICATION),
+                    CephalodMathsProblem(listOf(328, 64, 98), ADDITION),
+                    CephalodMathsProblem(listOf(51, 387, 215), MULTIPLICATION),
+                    CephalodMathsProblem(listOf(64, 23, 314), ADDITION)
                 )
             ), parse(puzzleInput)
         )
     }
 
     private fun parse(puzzleInput: String): CephalodMaths {
-        val numbersInput = puzzleInput.split("\n").dropLast(1)
-        return CephalodMaths(
-            listOf(
-                CephalodMathsProblem(
-                    numbersInput.map { it.trim().toInt() },
-                    MULTIPLIED
-                )
-            )
-        )
+        val puzzleInputRows = puzzleInput.split("\n")
+        val numberInputRows = puzzleInputRows.dropLast(1).map { it.trim().split("\\s+".toRegex()) }
+        val operators = puzzleInputRows.last().split("\\s+".toRegex()).map {
+            when (it) {
+                "*" -> MULTIPLICATION
+                else -> ADDITION
+            }
+        }
+        val maximumRowSize = numberInputRows.maxOf { it.size }
+        val problems = (0 until maximumRowSize).map { rowIndex ->
+            CephalodMathsProblem(numberInputRows.mapNotNull { it[rowIndex].trim().toIntOrNull() }, operators[rowIndex])
+        }
+
+        return CephalodMaths(problems)
     }
 }
