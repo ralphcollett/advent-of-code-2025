5ec5cd35552fd560280dd1a36277784b61db8325
ralph.collett <ralph.collett@gmail.com>
2026-01-22 14:23:06 +0000

    Test deal with multiple splitters.

--- src/test/kotlin/day7/TachylonManifoldTest.kt
+++ src/test/kotlin/day7/TachylonManifoldTest.kt
@@ -65,19 +65,40 @@ class TachylonManifoldTest {
         assertEquals(moved, moveDown(initial))
     }
 
+    @Test
+    fun `Can deal with multiple splitters`() {
+        val initial = TachylonManifold(
+            listOf(
+                listOf(EMPTY, EMPTY, TACHYLON_BEAM, EMPTY, EMPTY),
+                listOf(EMPTY, TACHYLON_BEAM, SPLITTER, TACHYLON_BEAM, EMPTY),
+                listOf(EMPTY, SPLITTER, EMPTY, SPLITTER, EMPTY),
+            )
+        )
+
+        val moved = TachylonManifold(
+            listOf(
+                listOf(EMPTY, EMPTY, TACHYLON_BEAM, EMPTY, EMPTY),
+                listOf(EMPTY, TACHYLON_BEAM, SPLITTER, TACHYLON_BEAM, EMPTY),
+                listOf(TACHYLON_BEAM, SPLITTER, TACHYLON_BEAM, SPLITTER, TACHYLON_BEAM),
+            )
+        )
+
+        assertEquals(moved, moveDown(initial))
+    }
+
     private fun moveDown(initial: TachylonManifold): TachylonManifold {
         val grid = initial.grid
         val liveRowIndex = grid.indexOfLast { row -> row.any { it == TACHYLON_BEAM } }
         val liveRow = grid[liveRowIndex]
-        val tachylonPosition = liveRow.indexOfLast { it == TACHYLON_BEAM }
+        val tachylonPositions = liveRow.withIndex().filter { it.value == TACHYLON_BEAM }.map { it.index }
         val nextRow = grid[liveRowIndex + 1]
         val indexNextRow = nextRow.withIndex()
         val nextRowSplitterPositions = indexNextRow.filter { it.value == SPLITTER }.map { it.index }
         val newNextRow = nextRow.mapIndexed { index, cell ->
             when (cell) {
-                EMPTY if (index == tachylonPosition) -> TACHYLON_BEAM
-                EMPTY if nextRowSplitterPositions.contains(index + 1) -> TACHYLON_BEAM
-                EMPTY if nextRowSplitterPositions.contains(index - 1) -> TACHYLON_BEAM
+                EMPTY if (index in tachylonPositions) -> TACHYLON_BEAM
+                EMPTY if (index + 1 in tachylonPositions) && index + 1 in nextRowSplitterPositions -> TACHYLON_BEAM
+                EMPTY if (index - 1 in tachylonPositions) && index - 1 in nextRowSplitterPositions -> TACHYLON_BEAM
                 SPLITTER -> SPLITTER
                 else -> EMPTY
             }
