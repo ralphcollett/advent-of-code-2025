4db014e6fddee248257eced27a4afbbbdef56a22
ralph.collett <ralph.collett@gmail.com>
2026-01-22 14:40:25 +0000

    Count splits.

--- src/test/kotlin/day7/TachylonManifoldTest.kt
+++ src/test/kotlin/day7/TachylonManifoldTest.kt
@@ -125,4 +125,28 @@ class TachylonManifoldTest {
 
         assertEquals(moved, initial.moveDown())
     }
+
+    @Test
+    fun `Counts splits`() {
+        assertEquals(
+            3, TachylonManifold(
+                listOf(
+                    listOf(EMPTY, EMPTY, TACHYLON_BEAM, EMPTY, EMPTY),
+                    listOf(EMPTY, TACHYLON_BEAM, SPLITTER, TACHYLON_BEAM, EMPTY),
+                    listOf(TACHYLON_BEAM, SPLITTER, TACHYLON_BEAM, SPLITTER, TACHYLON_BEAM),
+                )
+            ).splits()
+        )
+    }
+}
+
+private fun TachylonManifold.splits(): Int {
+    return grid.takeWhile { it.contains(TACHYLON_BEAM) }.mapIndexed { rowNumber, row ->
+        when {
+            rowNumber >= grid.size - 1 -> 0
+            else -> row.zip(grid[rowNumber + 1]).count { (currentRowCell, nextRowCell) ->
+                currentRowCell == TACHYLON_BEAM && nextRowCell == SPLITTER
+            }
+        }
+    }.sum()
 }
