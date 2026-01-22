e2bca5f3461372812b43c0ffc78333ec4665383d
ralph.collett <ralph.collett@gmail.com>
2026-01-22 08:45:15 +0000

    Check freshness test.

--- src/test/kotlin/day5/CafeteriaTest.kt
+++ src/test/kotlin/day5/CafeteriaTest.kt
@@ -1,27 +1,39 @@
 package day5
 
 import day5.IngredientFreshness.FRESH
+import day5.IngredientFreshness.SPOILED
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CafeteriaTest {
 
     @Test
-    fun `Knows if spoiled ingredient based on range`() {
+    fun `Knows if fresh ingredient based on range`() {
         assertEquals(FRESH, freshness(4, FreshIngredientRange(3, 5)))
     }
 
     @Test
-    fun `Knows if spoiled ingredient based on range including lower boundary`() {
+    fun `Knows if fresh ingredient based on range including lower boundary`() {
         assertEquals(FRESH, freshness(3, FreshIngredientRange(3, 5)))
     }
 
     @Test
-    fun `Knows if spoiled ingredient based on range including upper boundary`() {
+    fun `Knows if fresh ingredient based on range including upper boundary`() {
         assertEquals(FRESH, freshness(3, FreshIngredientRange(3, 5)))
     }
 
+    @Test
+    fun `Knows if spoiled ingredient`() {
+        assertEquals(SPOILED, freshness(2, FreshIngredientRange(3, 5)))
+    }
+
     private fun freshness(ingredientId: FreshIngredientId, freshIngredientRange: FreshIngredientRange): IngredientFreshness {
-        return FRESH
+        val idInRange = freshIngredientRange.run {
+            startId..endId
+        }.contains(ingredientId)
+        return when {
+            idInRange -> FRESH
+            else -> SPOILED
+        }
     }
 }
