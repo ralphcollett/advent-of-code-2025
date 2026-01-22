d37eeca8fdfbd70cf2ae79839492430540ad3590
ralph.collett <ralph.collett@gmail.com>
2026-01-22 10:08:23 +0000

    Test calculates multiple problems.

--- src/test/kotlin/day6/CephalodMathTest.kt
+++ src/test/kotlin/day6/CephalodMathTest.kt
@@ -9,36 +9,57 @@ class CephalodMathTest {
 
     @Test
     fun `Performs addition`() {
-        val calculation = calculate(CephalodMath(
-            listOf(
-                CephalodMathProblem(
-                    listOf(328, 64, 98),
-                    ADDITION
+        val calculation = calculate(
+            CephalodMath(
+                listOf(
+                    CephalodMathProblem(
+                        listOf(328, 64, 98),
+                        ADDITION
+                    )
                 )
             )
-        ))
+        )
 
         assertEquals(490, calculation)
     }
 
     @Test
     fun `Performs multiplication`() {
-        val calculation = calculate(CephalodMath(
-            listOf(
-                CephalodMathProblem(
-                    listOf(123, 45, 6),
-                    MULTIPLICATION
+        val calculation = calculate(
+            CephalodMath(
+                listOf(
+                    CephalodMathProblem(
+                        listOf(123, 45, 6),
+                        MULTIPLICATION
+                    )
                 )
             )
-        ))
+        )
 
         assertEquals(33210, calculation)
     }
 
+    @Test
+    fun `Gets total of multiple calculations`() {
+        val calculation = calculate(
+            CephalodMath(
+                listOf(
+                    CephalodMathProblem(listOf(123, 45, 6), MULTIPLICATION),
+                    CephalodMathProblem(listOf(328, 64, 98), ADDITION),
+                    CephalodMathProblem(listOf(51, 387, 215), MULTIPLICATION),
+                    CephalodMathProblem(listOf(64, 23, 314), ADDITION)
+                )
+            )
+        )
+
+        assertEquals(4277556, calculation)
+    }
+
     private fun calculate(cephalodMath: CephalodMath): Int {
-        val (numbers, operator) = cephalodMath.problems.first()
-        return numbers.reduce { acc, i ->
-            operator.operate(acc, i)
+        return cephalodMath.problems.sumOf { (numbers, operator) ->
+            numbers.reduce { acc, i ->
+                operator.operate(acc, i)
+            }
         }
     }
 }
