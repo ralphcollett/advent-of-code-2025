a4d5012a13d42e67226b6947b10c5c9e3702a0a8
ralph.collett <ralph.collett@gmail.com>
2026-01-22 08:02:55 +0000

    Parsing test with multiple ID ranges.

--- src/test/kotlin/day5/CafeteriaParserTest.kt
+++ src/test/kotlin/day5/CafeteriaParserTest.kt
@@ -15,18 +15,29 @@ class CafeteriaParserTest {
     fun `Parses puzzle input to database`() {
         val puzzleInput = """
             3-5
+            10-14
+            16-20
+            12-18
         """.trimIndent()
 
         assertEquals(CafeteriaDatabase(
-            listOf(FreshIngredientRange(3, 5)),
+            listOf(
+                FreshIngredientRange(3, 5),
+                FreshIngredientRange(10, 14),
+                FreshIngredientRange(16, 20),
+                FreshIngredientRange(12, 18)
+            ),
             emptyList()
         ), parse(puzzleInput))
     }
 
     private fun parse(puzzleInput: String): CafeteriaDatabase {
-        val (startId, endId) = puzzleInput.split("-")
+        val freshIngredientIdRanges = puzzleInput.split("\n").map { puzzleRow ->
+            val (startId, endId) = puzzleRow.split("-")
+            FreshIngredientRange(startId.toInt(), endId.toInt())
+        }
         return CafeteriaDatabase(
-            listOf(FreshIngredientRange(startId.toInt(), endId.toInt())),
+            freshIngredientIdRanges,
             emptyList()
         )
     }
