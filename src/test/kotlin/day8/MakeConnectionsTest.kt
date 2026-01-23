cca1e469557f26444593ec4af3bebeed15843c4d
ralph.collett <ralph.collett@gmail.com>
2026-01-23 07:11:39 +0000

    Test build circuits.

--- src/test/kotlin/day8/MakeConnectionsTest.kt
+++ src/test/kotlin/day8/MakeConnectionsTest.kt
@@ -71,6 +71,11 @@ class MakeConnectionsTest {
     }
 
     @Test
+    fun `Does not allow connections if already in same circuit`() {
+
+    }
+
+    @Test
     fun `Find distance between 2 junction boxes`() {
         // SQRT ( (162 - 57)^2 + (817 - 618)^2 + (812 - 52)^2 )
         val distance = JunctionBox(162, 817, 812).distanceTo(
@@ -78,5 +83,45 @@ class MakeConnectionsTest {
         )
 
         assertEquals(787.8, distance, absoluteTolerance = 0.1)
+    }
+
+    @Test
+    fun `Finds circuits`() {
+        val connectedJunctionBoxes = listOf(
+            ConnectedJunctionBoxes(JunctionBox(1, 1, 1), JunctionBox(2, 2, 2)),
+            ConnectedJunctionBoxes(JunctionBox(2, 2, 2), JunctionBox(3, 3, 3)),
+            ConnectedJunctionBoxes(JunctionBox(4, 4, 4), JunctionBox(5, 5, 5)),
+        )
+
+        val circuits = circuits(connectedJunctionBoxes)
+
+        assertEquals(
+            listOf(
+                setOf(JunctionBox(1, 1, 1), JunctionBox(2, 2, 2), JunctionBox(3, 3, 3)),
+                setOf(JunctionBox(4, 4, 4), JunctionBox(5, 5, 5)),
+            ), circuits
+        )
+    }
+
+    private fun circuits(connectedJunctionBoxes: List<ConnectedJunctionBoxes>): List<Set<JunctionBox>> {
+        return buildCircuits(connectedJunctionBoxes, emptyList())
+    }
+
+    private fun buildCircuits(connectedJunctionBoxes: List<ConnectedJunctionBoxes>, circuits: List<Set<JunctionBox>>): List<Set<JunctionBox>> {
+        if (connectedJunctionBoxes.isEmpty()) return circuits
+
+        val connectedJunctionBox = connectedJunctionBoxes.first()
+
+        val updatedCircuits: List<Set<JunctionBox>> =
+            when {
+                circuits.none { circuit -> connectedJunctionBox.junctionBoxA in circuit || connectedJunctionBox.junctionBoxB in circuit }
+                    -> circuits + setOf((setOf(connectedJunctionBox.junctionBoxA, connectedJunctionBox.junctionBoxB)))
+                else -> circuits.map { circuit ->
+                    if (circuit.any { it == connectedJunctionBox.junctionBoxA || it == connectedJunctionBox.junctionBoxB }) circuit + connectedJunctionBox.junctionBoxA + connectedJunctionBox.junctionBoxB
+                    else circuit
+                }
+            }
+
+        return buildCircuits(connectedJunctionBoxes - connectedJunctionBox, updatedCircuits)
     }
 }
