2df2e444223fccffa139afd01d8d3c8b4cbaa58b
ralph.collett <ralph.collett@gmail.com>
2026-01-23 09:37:57 +0000

    First acceptance test.

---
+++ src/test/kotlin/day9/MovieTheater.kt
@@ -0,0 +1,5 @@
+package day9
+
+fun findLargestRectangle(puzzleInput: String): Int {
+    TODO("Not yet implemented")
+}
---
+++ src/test/kotlin/day9/MovieTheaterAcceptanceTest.kt
@@ -0,0 +1,29 @@
+package day9
+
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class MovieTheaterAcceptanceTest {
+    
+    @Test
+    @Ignore
+    fun `Finds biggest rectangle with red tiles for corners`() {
+        val puzzleInput = """
+            7,1
+            11,1
+            11,7
+            9,7
+            9,5
+            2,5
+            2,3
+            7,3
+        """.trimIndent()
+
+        val area = findLargestRectangle(puzzleInput)
+
+        assertEquals(50, area)
+    }
+
+}
