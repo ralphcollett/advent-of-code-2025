d7666b1ae0b919ea85fea34ebd73945522c3f335
ralph.collett <ralph.collett@gmail.com>
2026-01-23 04:01:01 +0000

    Start creating domain

--- src/main/kotlin/day8/JunctionBox.kt
+++ src/main/kotlin/day8/JunctionBox.kt
@@ -1,7 +1,17 @@
 package day8
 
+typealias ConnectedJunctionBoxes = Pair<JunctionBox, JunctionBox>
+
 fun circuitSize(puzzleInput: String): Int {
+    val junctionBoxes = parse(puzzleInput)
+    makeConnections(junctionBoxes, 10)
     TODO("Not yet implemented")
 }
+
+fun makeConnections(junctionBoxes: List<JunctionBox>, numberOfLightStrings: Int) {
+    TODO("Not yet implemented")
+}
+
+data class JunctionBoxesCircuits(val junctionBoxes: List<JunctionBox>, val connectedJunctionBoxes: List<ConnectedJunctionBoxes>)
 
 data class JunctionBox(val x: Int, val y: Int, val z: Int)
