b2dfb225a94cb1e04055637bae4ed525533f6df6
ralph.collett <ralph.collett@gmail.com>
2026-01-24 08:01:48 +0000

    Check knows when no paths.

---
+++ src/test/kotlin/day11/DeviceTest.kt
@@ -0,0 +1,20 @@
+package day11
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import kotlin.test.Test
+
+class DeviceTest {
+
+    @Test
+    fun `Knows no paths from you to end when no devices`() {
+        val devices = emptyList<Device>()
+
+        val paths = pathsFromYouToEnd(devices)
+
+        assertEquals(emptyList<List<String>>(), paths)
+    }
+
+    private fun pathsFromYouToEnd(devices: List<Device>): List<List<String>> {
+        return emptyList()
+    }
+}
